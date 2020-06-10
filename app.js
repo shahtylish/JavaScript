ecfb232e373aae0d1c812c1b9f966d7b9a94995e
shahtylish <shahwajid.sk@gmail.com>
2020-06-10 16:27:27 +0500

    JavaScript Assignment

--- app.js
+++ app.js
@@ -94,10 +94,11 @@ var variable1, variable2, variable3;
 
 //Task 2:
 var legal_variable_1, legal_variable_2, legal_variable_3, legal_variable_4, legal_variable_5;
-var illegal_variable_1, illegal_variable_2, illegal_variable_3, illegal_variable_4, illegal_variable_5;
+//var 1illegal_variable_1, !illegal_variable_2, @illegal_variable_3, *illegal_variable_4, )illegal_variable_5;
 
 //Task 3 is on Index.html page.
 
+document.write("<br>");
 document.write("<br>");
 document.write("<br>");
 
@@ -494,7 +495,6 @@ if('car' < 'cat'){
     alert("Car is smaller than cat");
 }
 
-
 //Task 7:
 var secret_number = 7;
 var guess = +prompt("Guess the secret number:");
@@ -559,3 +559,336 @@ if(operators === '/'){
 if(operators === '%'){
     alert(fnumber + " % " + snumber + " is: " + (fnumber%snumber));
 }
+
+document.write("<br><br>");
+
+//Chapters # 12-13:
+//Task 1:
+var character = prompt("Enter a character:");
+var capitalA = String.fromCharCode(65);
+var capitalZ = String.fromCharCode(90);
+var smalla = String.fromCharCode(97);
+var smallz = String.fromCharCode(122);
+var num1 = String.fromCharCode(48);
+var num2 = String.fromCharCode(57);
+
+if(character >= capitalA && character <= capitalZ){
+    alert("The given input is Uppercase Letter.");
+}
+else if(character >= smalla && character <= smallz){
+    alert("The given input is Lowercase Letter.");
+}
+else if(character >= num1 && character <= num2){
+    alert("The given input is Number.");
+}
+else{
+    alert("You enter a wrong character.");
+}
+
+//Task 2:
+var int1 = +prompt("Enter a first integer:");
+var int2 = +prompt("Enter a second integer:");
+if(int1 > int2){
+    alert("First integer is larger.");
+}
+else if(int1 < int2){
+    alert("Second integer is larger.");
+}
+else if(int1 === int2){
+    alert("Both integers are equal.");
+}
+
+//Task 3:
+var number = +prompt("Enter a number:");
+if(number < 0){
+    alert("The number is Negative.");
+}
+else if(number > 0){
+    alert("The number is Positive.");
+}
+else if(number === 0){
+    alert("The number is Zero.");
+}
+
+//Task 4:
+var vowel = prompt("Enter a string of length 1:");
+if(vowel === 'a'){
+    alert("True.");
+}
+else if(vowel === 'e'){
+    alert("True.");
+}
+else if(vowel === 'i'){
+    alert("True.");
+}
+else if(vowel === 'o'){
+    alert("True.");
+}
+else if(vowel === 'u'){
+    alert("True.");
+}
+else{
+    alert("False");
+}
+
+//Task 5:
+var correct_pass = "Correct";
+var password = prompt("Please enter your password:");
+if(password === ""){
+        alert("Please enter your Password!");
+}
+else if(password === correct_pass){
+    alert("Correct!");
+}
+else{
+    alert("Incorrect password!");
+}
+
+//Task 6:
+var greeting;
+var hour = 13;
+if(hour < 18){
+    greeting = "Good day";
+    alert(greeting);
+}
+else{
+    greeting = "Good evening";
+    alert(greeting);
+}
+
+//Task 7:
+var time = +prompt("Enter time in 24 hours clock format, (Like 7pm = 1900):");
+if(time >= 0000 && time < 1200){
+    alert("Good Morning!");
+}
+else if(time >= 1200 && time < 1700){
+    alert("Good Afternoon!");
+}
+else if(time >= 1700 && time < 2100){
+    alert("Good Evening!");
+}
+else if(time >= 2100 && time <= 2359){
+    alert("Good Night!");
+}
+
+//Chapters # 14-16:
+//Task 1:
+var arr1 = [];
+
+//Task 2:
+var arr2 = {};
+
+//Task 3:
+var str_arr = ['Fuits', 'Vegetables', 'Dry Fruits'];
+
+//Task 4:
+var num_arr = [1, 2, 3, 4, 5];
+
+//Task 5:
+var bool_arr = [true, false];
+
+//Task 6:
+var mix_arr = ['Apples', 5, 'Mangoes', 10, true, 'Bananas', 7, false];
+
+document.write("<br><br>");
+
+//Task 7:
+var qual = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+document.write(qual[0] + "<br>");
+document.write(qual[1] + "<br>");
+document.write(qual[2] + "<br>");
+document.write(qual[3] + "<br>");
+document.write(qual[4] + "<br>");
+document.write(qual[5] + "<br>");
+document.write(qual[6] + "<br>");
+document.write(qual[7] + "<br>");
+
+document.write("<br><br>");
+
+//Task 8:
+var Snames = ['Micheal', 'John', 'Tony'];
+var scores = [320, 230, 480];
+var total_score = 500;
+document.write("Score of " + Snames[0] + " is " + scores[0] + ". Percentage: " + (scores[0]/total_score * 100) + "% <br>");
+document.write("Score of " + Snames[1] + " is " + scores[1] + ". Percentage: " + (scores[1]/total_score * 100) + "% <br>");
+document.write("Score of " + Snames[2] + " is " + scores[2] + ". Percentage: " + (scores[2]/total_score * 100) + "% <br>");
+
+document.write("<br><br>");
+
+//Task 10:
+var Sscores = [320, 230, 480, 120];
+document.write(Sscores.sort());
+
+document.write("<br><br>");
+
+//Task 11:
+var cities = ['Karachi', 'Lahore', 'Islamabad', 'Quetta', 'Peshawar'];
+document.write('Cities list: ' + cities +"<br>");
+var selectedCities = cities.slice(2, 4);
+document.write('Selected cities list: ' + selectedCities);
+
+document.write("<br><br>");
+
+//Task 12:
+var join_arr = ['This', 'is', 'my', 'cat'];
+document.write('Array: ' + join_arr + "<br>");
+document.write('String: ' + join_arr.join(" "));
+
+document.write("<br><br>");
+
+//Task 13:
+var devices = ['Keyboard', 'Mouse', 'Printer', 'Monitor'];
+document.write('Devices: ' + devices);
+
+document.write("<br><br>");
+
+//Task 14:
+var devices = ['Keyboard', 'Mouse', 'Printer', 'Monitor'];
+document.write('Reverse Devices: ' + devices.reverse());
+
+document.write("<br><br>");
+
+//Task 15:
+var brands = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+document.write("<select>");
+document.write("<option>" + brands[0] + "</option>");
+document.write("<option>" + brands[1] + "</option>");
+document.write("<option>" + brands[2] + "</option>");
+document.write("<option>" + brands[3] + "</option>");
+document.write("<option>" + brands[4] + "</option>");
+document.write("<option>" + brands[5] + "</option>");
+document.write("<select>");
+
+document.write("<br><br>");
+
+//Chapters # 17-20:
+//Task 1:
+var multi_arr = [[]];
+
+//Task 2:
+var matrix = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+document.write(matrix[0].join(" ") + "<br>" + matrix[1].join(" ") + "<br>" + matrix[2].join(" "));
+
+document.write("<br><br>");
+
+//Task 3:
+for(var i = 1; i <= 10; i++){
+    document.write(i + "<br>");
+}
+
+document.write("<br><br>");
+
+//Task 4:
+var multi_table = +prompt("Enter a number to show its multiplication table:");
+var table_length = +prompt("Enter length of multiplication table:");
+for(var i = 1; i <= table_length; i++){
+    document.write(multi_table + " x " + i + " = " + i*multi_table + "<br>");
+}
+
+document.write("<br><br>");
+
+//Task 5:
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+for(var i = 0; i < fruits.length; i++){
+    document.write(fruits[i] + "<br>");
+}
+document.write("<br>");
+for(var j = 0; j < fruits.length; j++){
+    document.write("Element at index " + j + " is " + fruits[j] + "<br>");
+}
+
+document.write("<br><br>");
+
+//Task 6:
+document.write("Counting: <br>");
+for(var count = 1; count <= 15; count++){
+    document.write(count + ", ");
+}
+
+document.write("<br> Reverse Counting: <br>");
+for(var rcount = 10; rcount >= 1; rcount--){
+    document.write(rcount + ", ");
+}
+
+document.write("<br> Even: <br>");
+for(var even = 0; even <= 20; even += 2){
+    document.write(even + ", ");
+}
+
+document.write("<br> Odd: <br>");
+for(var odd = 1; odd <= 19; odd += 2){
+    document.write(odd + ", ");
+}
+
+document.write("<br> Series: <br>");
+for(var even = 2; even <= 20; even += 2){
+    document.write(even + "k, ");
+}
+
+document.write("<br><br>");
+
+//Task 7:
+var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+var search = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+var matchFound = false;
+for(var i = 0; i < A.length; i++){
+    if(search === A[i]){
+        matchFound = true;
+        alert(A[i] + " is available at index " + i + " in our bakery.");
+        break;
+    }
+}
+if(matchFound === false){
+    alert("We are sorry. " + search + " is not available in our bakery.");
+}
+
+//Task 8:
+var num = [24, 53, 78, 91, 12];
+var larg_num = num[0];
+for(var i = 0; i < num.length; i++){
+    if(larg_num < num[i]){
+        larg_num = num[i];
+    }
+}
+document.write("Array items: " + num + "<br>");
+document.write("The largest number is " + larg_num);
+
+document.write("<br><br>");
+
+//Task 9:
+var num = [24, 53, 78, 91, 12];
+var larg_num = num[0];
+for(var i = 0; i < num.length; i++){
+    if(larg_num > num[i]){
+        larg_num = num[i];
+    }
+}
+document.write("Array items: " + num + "<br>");
+document.write("The smallest number is " + larg_num);
+
+document.write("<br><br>");
+
+//Task 10:
+for(var i = 5; i <= 100; i += 5){
+    document.write(i + ",");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
